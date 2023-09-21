f884f51af542b689fa527bd7e7b6e4f2106d9ac7
Hao Ni <nhchristine@outlook.com>
2023-09-21 13:36:34 +1000

    edit graphical banner for display issue
    Signed-off-by: Hao Ni <nhchristine@outlook.com>

--- new_cross_app/lib/Home Page/graphical_banner.dart
+++ new_cross_app/lib/Home Page/graphical_banner.dart
@@ -7,42 +7,43 @@ import 'package:sizer/sizer.dart';
 
 /// Reusable widget which is primarily used for aesthetics.
 class GraphicalBanner extends StatelessWidget {
-
   //static bannerHeight = MediaQuery.of(context).size.height*0.5;
   const GraphicalBanner({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: MediaQuery.of(context).size.height*0.2,
+        height: MediaQuery.of(context).size.height * 0.2,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
             image: const DecorationImage(
                 image: AssetImage("assets/images/banner_background.png"),
-                fit: BoxFit.fill
-            ),
+                fit: BoxFit.fill),
             boxShadow: defaultShadows,
             borderRadius: BorderRadius.circular(HomeState.borderRadius)),
 
         // AnimatedText container
         child: Container(
-          child: Align(
-            alignment: const Alignment(0,-.55),
-            child:
-            DefaultTextStyle(
-                style: GoogleFonts.parisienne(fontSize: 20.sp,color: Colors.black),
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    TypewriterAnimatedText("Jemma",speed: const Duration(milliseconds: 500)),
-                    TypewriterAnimatedText("Life made easier.",speed: const Duration(milliseconds: 150)),
-                    TypewriterAnimatedText("Get things fixed quicker.",speed: const Duration(milliseconds: 150))
-                  ],
-                  pause: const Duration(seconds: 5),
-                  repeatForever: true,
-                )
+          child: FittedBox(
+            child: Align(
+              alignment: const Alignment(0, -.55),
+              child: DefaultTextStyle(
+                  style: GoogleFonts.parisienne(
+                      fontSize: 20.sp, color: Colors.black),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText("Jemma",
+                          speed: const Duration(milliseconds: 500)),
+                      TypewriterAnimatedText("Life made easier.",
+                          speed: const Duration(milliseconds: 150)),
+                      TypewriterAnimatedText("Get things fixed quicker.",
+                          speed: const Duration(milliseconds: 150))
+                    ],
+                    pause: const Duration(seconds: 5),
+                    repeatForever: true,
+                  )),
             ),
           ),
-        ),
-      );
+        ));
   }
 }
