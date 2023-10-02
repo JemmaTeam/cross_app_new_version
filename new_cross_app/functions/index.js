811eb348483eebd7ebba03aa982bf7e957ebdf01
Jingcheng Su(Alex) <759766900@qq.com>
2023-10-03 09:49:32 +1100

    stripe notification is under development.

--- new_cross_app/functions/index.js
+++ new_cross_app/functions/index.js
@@ -442,6 +442,61 @@ exports.monitorNewMessages = functions.firestore
         return null;
     });
 
+// Using Express
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+app.use(express.json());
+
+// Use JSON parser for all non-webhook routes
+app.use((req, res, next) => {
+  if (req.originalUrl === "/webhook") {
+    next();
+  } else {
+    bodyParser.json()(req, res, next);
+  }
+});
+
+// Set your secret key. Remember to switch to your live secret key in production!
+// See your keys here: https://dashboard.stripe.com/apikeys
+const Stripe = require('stripe');
+const stripe = Stripe('sk_test_51MxqKoCLNEXP0Gmv34Ixc05ATpLLTkXxK1VmLe4rng6eaiPqiyiDn5iYhaeGA9iZXEdDYIEDZDuTQMMvy4lRKW3J003L5D13iI');
+
+// If you are testing your webhook locally with the Stripe CLI you
+// can find the endpoint's secret by running `stripe listen`
+// Otherwise, find your endpoint's secret in your webhook settings in the Developer Dashboard
+const endpointSecret = 'whsec_...';
+
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  // Verify webhook signature and extract the event.
+  // See https://stripe.com/docs/webhooks#verify-events for more information.
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  } catch (err) {
+    return response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  if (event.type === 'account.application.deauthorized') {
+    const application = event.data.object;
+    const connectedAccountId = event.account;
+    handleDeauthorization(connectedAccountId, application);
+  }
+
+  response.json({received: true});
+});
+
+const handleDeauthorization = (connectedAccountId, application) => {
+  // Clean up account state.
+  console.log('Connected account ID: ' + connectedAccountId);
+  console.log(JSON.stringify(application));
+}
+
+app.listen(4242, () => console.log(`Node server listening on port ${4242}!`));
+
 
 // move from server.js
 // Connect webhook notification function 1.Define a new Firebase Cloud Function to handle webhooks:
