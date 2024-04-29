f04312d08a31945518d41ba00ea82129b97b9d2e
u7568823 <u7568823@anu.edu.au>
2024-04-29 22:44:46 +1000

    nothing

--- new_cross_app/lib/Login/login.dart
+++ new_cross_app/lib/Login/login.dart
@@ -32,8 +32,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginState extends State<LoginPage> {
 // Controllers for email and password
-  // final TextEditingController emailController = TextEditingController();
-  // final TextEditingController passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
 //keep it ! no deletion!consumer test account
   // final TextEditingController emailController =
@@ -42,10 +42,10 @@ class _LoginState extends State<LoginPage> {
   //     TextEditingController(text: "123456");
 
 //keep it! no deletion!tradie test account
-  final TextEditingController emailController =
-      TextEditingController(text: "tben@outlook.com");
-  final TextEditingController passwordController =
-      TextEditingController(text: "123456");
+  // final TextEditingController emailController =
+  //     TextEditingController(text: "tben@outlook.com");
+  // final TextEditingController passwordController =
+  //     TextEditingController(text: "123456");
 
   // Loading state
   bool _isLoading = false;
